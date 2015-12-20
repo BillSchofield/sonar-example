a5336ae5db1ccbd428277faba32f9b67e93cac45
Bill Schofield <bill@billschofield.net>
2015-12-20 10:36:20 -0500

    Added more people to set and rm'ed iml file

--- src/main/java/com/thoughtworks/sonarexample/Main.java
+++ src/main/java/com/thoughtworks/sonarexample/Main.java
@@ -5,9 +5,14 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Person(0, "Bill", "Schofield");
         Set<Person> personSet = new HashSet<Person>();
 
-        personSet.add(person);
+        personSet.add(new Person(0, "Bill", "Schofield"));
+        personSet.add(new Person(0, "Bill", "Schofield"));
+        personSet.add(new Person(1, "Carol", "Schofield"));
+
+        for (Person person : personSet) {
+            System.out.println(person.getFirstName() + person.getLastName());
+        }
     }
 }
